9c45e794065ba4504ff5deb406460a6db0bc834a
Patrick Lee <pelee@ucdavis.edu>
2022-03-21 16:02:21 -0700

    added betting to points cog and updated config

--- cogs/points.py
+++ cogs/points.py
@@ -1,8 +1,7 @@
 from unicodedata import name
 import discord
 from discord.ext import commands, tasks
-import requests
-import json
+import math
 import psycopg2
 from config import config
 import os
@@ -12,8 +11,6 @@ from dotenv import load_dotenv
 load_dotenv()
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 GUILD_ID = os.getenv('GUILD_ID')
-
-print(POSTGRES_PASSWORD)
 
 accepted_users = []
 
@@ -40,7 +37,11 @@ class Points(commands.Cog):
         self.stream_id_List = []
         self.someone_streaming = False
         self.bets = {}
+        # {discord_id : [bet_amount, bet_side]}
+        self.cur_bet = None
         self.betting_pool = 0
+        self.bets_is_allowed = False
+        self.streamer_id = None
         self.is_bet_active = False
         self.give_points.start()
 
@@ -92,6 +93,119 @@ class Points(commands.Cog):
             to_strings += (to_string)
         await ctx.channel.send('```\n' + to_strings + '\n```')
         cur.close()
+
+    @commands.command(name='payout_bet', help='Payout the current bet.')
+    async def payout_bet(self, ctx, result : int):
+        print(ctx.author.id)
+        print(self.streamer_id)
+        if int(ctx.author.id) == int(self.streamer_id) and self.is_bet_active:
+            for user, bet in self.bets.items():
+                if(bet[1] == result):
+                    # change payout value here
+                    payout = math.floor(0.8 * bet[0])
+                else:
+                    payout = -1 * bet[0]
+                cur = conn.cursor() 
+                command = f"UPDATE \"ChannelPoints\" SET total_points = total_points + {payout} WHERE discord_id = '{user}';"
+                cur.execute(command)
+                conn.commit()
+                cur.close()
+            self.is_bet_active = False
+            self.bets.clear()
+            self.cur_bet = None
+            self.streamer_id = None
+            self.betting_pool = 0
+            ctx.channel.send('Bets have been payed out.')
+        else:
+            await ctx.channel.send('Error paying out bet, either you are not the streamer or there is no bet active.')
+
+
+    @payout_bet.error
+    async def payout_bet_error(self, ctx, error):
+        if isinstance(error, commands.CommandError):
+            await ctx.channel.send('Forgot to include payout result (1 = Yes, 2 = No).')
+
+    @commands.command(name='start_bet', help='Start a bet')
+    async def start_bet(self, ctx, streamer_id_input, *bet):
+        bet_identity = bet
+
+        if (self.is_bet_active):
+            await ctx.channel.send(f'Bet is currently active, please payout before making a new one')
+        else:
+            streamer_id = streamer_id_input.lstrip("<@!")
+            streamer_id = streamer_id.rstrip(">") 
+            self.streamer_id = streamer_id
+            self.is_bet_active = True
+            self.bets_is_allowed = True
+            self.bets = {}
+            self.betting_pool = 0
+            to_string = ""
+            for word in bet_identity:
+                to_string += word + " "
+            self.cur_bet = to_string
+            await ctx.channel.send(f'Starting bet now: {to_string}, you have 20 seconds to bet.')
+            await ctx.channel.send (f'Remember: Command goes !join_bet <amount> <yes = 1/no = 2>')
+            await asyncio.sleep(10)
+            await ctx.channel.send(f'10 seconds left to bet')
+            await asyncio.sleep(10)
+            await ctx.channel.send(f'Bets are now closed')
+            self.bets_is_allowed = False
+
+    @start_bet.error
+    async def start_bet_error(self, ctx, error):
+        if isinstance(error, commands.CommandError):
+            await ctx.channel.send('Some error in starting bet, make sure you are doing !start_bet <@streamer> <what is the bet?>.')
+
+
+    @commands.command(name='get_bet', help='Get the current bet')
+    async def get_bet(self, ctx):
+        if self.is_bet_active:
+            await ctx.channel.send(f'{self.cur_bet}')
+            person = self.client.get_user(int(self.streamer_id))
+            await ctx.channel.send(f'Current streamer: {person}')
+        else:
+            await ctx.channel.send('No bets are currently active')
+
+    @commands.command(name='join_bet', help='Join a bet')
+    async def join_bet(self, ctx, amount : int, side : int):
+        if self.bets_is_allowed:
+            bet_amount = amount
+            discord_id = ctx.author.id
+            at_user = f'<@{discord_id}>'
+            if(amount <= 0):
+                ctx.channel.send(f'{at_user} Invalid bet: You cannot bet a negative amount.')
+        
+            elif side != 1 or side != 2:
+                ctx.channel.send(f'{at_user} Invalid bet: The side can only be 1 or 2.')
+
+            else:
+                cur = conn.cursor()
+                command = (f"SELECT total_points FROM \"ChannelPoints\" WHERE discord_id = \'{discord_id}\'")
+                cur.execute(command)
+                point_count = cur.fetchone()[0]
+                cur.execute(command)
+                cur.close()
+                if(not discord_id in self.bets):
+                    if(point_count >= bet_amount):
+                        print('bet is valid')
+                        self.bets[discord_id] = [bet_amount, side]
+                        await ctx.channel.send(f'Sucessfully created a bet of {bet_amount} for {at_user}.')
+                    else:
+                        await ctx.channel.send(f'{at_user} Invalid bet: you bet more points than you have')
+                else:
+                    if(point_count >= bet_amount + self.bets[discord_id][0]):
+                        print('bet is valid')
+                        self.bets[discord_id][0] += bet_amount
+                        await ctx.channel.send(f'Sucessfully increased bet of {at_user} to a total of {self.bets[discord_id]}')
+                    else:
+                        await ctx.channel.send(f'{at_user} Invalid bet: you bet more points than you have')
+        else:
+            await ctx.channel.send('Sorry, bets are currently closed.')
+
+    @join_bet.error
+    async def join_bet_error(self, ctx, error):
+        if isinstance(error, commands.CommandError):
+            await ctx.channel.send('Forgot to include a bet amount or side of the bet (1 or 2)')
 
     @commands.command(name='update_streams', help='Manually update stream status.')
     @commands.has_permissions(kick_members=True)
